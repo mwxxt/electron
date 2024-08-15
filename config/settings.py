a734f88e7d04380ecf1c3d342a1f435aaee21fa7
mwxxt <smanovmadiyar@gmail.com>
2024-08-15 18:12:14 +0600

    add migrations

--- config/settings.py
+++ config/settings.py
@@ -136,9 +136,9 @@ DATABASES = {
 
 MIGRATION_MODULES = {
     "user": "config.migrations.user_m",
-    "univer.employee": "config.migrations.employee_m",
-    "univer.student": "config.migrations.student_m",
-    "univer.branch": "config.migrations.branch_m",
+    "employee": "config.migrations.employee_m",
+    "student": "config.migrations.student_m",
+    "branch": "config.migrations.branch_m",
     "purchase": "config.migrations.purchase_m",
 }
 
